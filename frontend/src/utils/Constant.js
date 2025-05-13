9fe3deea7411e9446b28fc19bd676f82facc98c2
gaurav14082002 <gs1380525@gmail.com>
2025-05-13 13:24:34 -0400

    Update Constant.js

--- frontend/src/utils/Constant.js
+++ frontend/src/utils/Constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/users"
-export const JOBS_API_END_POINT = "http://localhost:8000/api/jobs"
-export const APPLY_API_END_POINT = "http://localhost:8000/api/application"
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/company"
+export const USER_API_END_POINT = "https://jobhunter-1-bt56.onrender.com/api/users"
+export const JOBS_API_END_POINT = "https://jobhunter-1-bt56.onrender.com/api/jobs"
+export const APPLY_API_END_POINT = "https://jobhunter-1-bt56.onrender.com/api/application"
+export const COMPANY_API_END_POINT = "https://jobhunter-1-bt56.onrender.com/api/company"
 
